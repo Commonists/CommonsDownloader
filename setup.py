5f3def6c0a3bab88341c3fa324a7426f7c5e4240
Jean-Fred <JeanFred@github>
2013-09-29 04:23:15 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+"""Setup script"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Utilities'
+]
+py_modules = ['commonsdownloader']
+scripts = ['download_from_Wikimedia_Commons.py']
+
+setup(
+      name='CommonsDownloader',
+      version=0.1,
+      author='Jean-Frédéric',
+      author_email='JeanFred@github',
+      url='http://github.com.org/JeanFred/CommonsDownloader',
+      description='Download thumbnails from Wikimedia Commons',
+      license='MIT',
+      py_modules=py_modules,
+      scripts=scripts,
+      classifiers=classifiers
+)
