ccbf5178ba938be65e377a5442416b77d5f1edc9
Jean-Fred <JeanFred@github>
2015-02-13 22:39:54 +0000

    Add requirements to setup.py
    The requirements have to also be present in the setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ classifiers = [
     'Topic :: Utilities'
 ]
 packages = ['commonsdownloader']
-requires = ['argparse']
+requires = ['argparse', 'mwclient', 'six']
 entry_points = {
         'console_scripts': [
             'download_from_Wikimedia_Commons = commonsdownloader.commonsdownloader:main',
